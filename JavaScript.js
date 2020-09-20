f46fed5ff6d644f59c8cee17a454adfa6d1098b6
Sebas572 <71107296+Sebas572@users.noreply.github.com>
2020-09-20 17:02:38 -0400

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -6,4 +6,13 @@ puntos="."
 puntos=puntos+".";
 }
 document.getElementById("Proximamente").innerHTML = "Proximamente"+puntos;
+var d = new Date();
+fecha1 = d.getDate();
+fecha2 = (d.getMonth()+1) + "/";
+fecha3 = d.getFullYear();
+hora1 = d.getHours() + ":";
+hora2 = d.getMinutes() + "";
+hora3 = d.getSeconds();
+document.getElementById("dia").innerHTML = "Nuevo "+(fecha1-19)+"d";
+
 }, 1000)
